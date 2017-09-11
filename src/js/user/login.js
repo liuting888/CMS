4225609f44b58b8bd24cf75f729c4e31d5d4db7b
liuting <397289891@qq.com>
2017-09-11 21:06:31 +0800

    左侧导航栏展示功能

--- src/js/user/login.js
+++ src/js/user/login.js
@@ -2,6 +2,7 @@ $('#login-form').ajaxForm({
     success: function(data) {
         if (data.code == 200) {
             alert('登陆成功');
+            localStorage.setItem('userinfo', JSON.stringify(data.result));
             location.href = '/dist';
         } else {
             alert('登陆失败');
