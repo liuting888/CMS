a7226d5df2d42f5cd29562e16403d783658d060b
liuting <397289891@qq.com>
2017-09-11 21:37:50 +0800

    个人中心数据显示

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -1,1 +1,11 @@
-
+require('../common/header.js');
+require('../common/aside.js');
+$.ajax({
+    url: '/v6/teacher/profile',
+    type: 'get',
+    success: function(data) {
+        if (data.code == 200) {
+            $('.teacher-profile').html(template('teacher-profile-tpl', data.result));
+        }
+    }
+});
