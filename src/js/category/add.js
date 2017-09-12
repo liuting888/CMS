5004b2d5712c21964b0c76efa160a6cde8e39894
liuting <397289891@qq.com>
2017-09-12 11:50:38 +0800

    完成添加学科

--- src/js/category/add.js
+++ src/js/category/add.js
@@ -1,2 +1,14 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+$.get('/v6/category/top', function(data) {
+    if (data.code == 200) {
+        $('#category-top-select').html(template('select-tpl', data.result));
+    }
+});
+
+$('#category-add-form').ajaxForm(function(data) {
+    if (data.code == 200) {
+        alert('恭喜你，创建了一个新学科');
+    }
+});
--- src/js/user/login.js
+++ src/js/user/login.js
@@ -1,3 +1,8 @@
+var userinfo = JSON.parse(localStorage.getItem('userinfo')) || {};
+var tc_avatar = userinfo.tc_avatar || '/public/img/default.png';
+$('.avatar img').attr('src', tc_avatar);
+
+
 $('#login-form').ajaxForm({
     success: function(data) {
         if (data.code == 200) {
