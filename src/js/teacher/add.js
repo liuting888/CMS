8801a95b9fa61a39e4597bb4ea1d900938993f8c
liuting <397289891@qq.com>
2017-09-12 17:12:59 +0800

    添加人员功能基本完成

--- src/js/teacher/add.js
+++ src/js/teacher/add.js
@@ -1,2 +1,9 @@
 require('../common/header.js');
 require('../common/aside.js');
+
+
+$('#teacher-add-form').ajaxForm(function(data) {
+    if (data.code == 200) {
+        alert('恭喜你，添加讲师成功');
+    }
+});
