73b411fd5a179030667ecf8e129e41661f2adf96
liuting <397289891@qq.com>
2017-09-13 19:40:04 +0800

    课程编辑第三部分数据回显完成

--- src/js/course/course_edit_step3.js
+++ src/js/course/course_edit_step3.js
@@ -1,3 +1,11 @@
 
 require('../common/header.js');
 require('../common/aside.js');
+var util = require('../common/util.js');
+var cs_id = util.getSearch('cs_id');
+$.get('/v6/course/lesson', { cs_id: cs_id }, function(data) {
+      if(data.code == 200) {
+        data.result.editIndex = 3;
+        $('#course-edit3').append(template('course-edit3-tpl', data.result));
+      }
+    });
