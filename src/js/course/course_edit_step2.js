1deca3b2a609308874916fbb0b880beeeb1b6017
liuting <397289891@qq.com>
2017-09-13 15:42:38 +0800

    课程管理第二步完成

--- src/js/course/course_edit_step2.js
+++ src/js/course/course_edit_step2.js
@@ -1,3 +1,10 @@
 
 require('../common/header.js');
 require('../common/aside.js');
+var util = require('../common/util.js'); 
+var cs_id = util.getSearch('cs_id');
+$.get('/v6/course/picture', { cs_id: cs_id }, function (data) {
+    if (data.code == 200) {
+        $('#course-edit2').append(template('course-edit2-tpl', data.result));
+    }
+});
